29729c8aa7ae8fe1491018b694a7e8527f4357df
dvbb <578760600@qq.com>
2024-09-01 13:27:48 +0800

    Add new assets

---
+++ Assets/Script/Skill/CrystalController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalController : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private Animator animator;
+    
+}
--- Assets/Script/SkillManager.cs
+++ Assets/Script/SkillManager.cs
@@ -9,7 +9,7 @@ public class SkillManager : MonoBehaviour
     public CloneSkill clone {  get; private set; }
     public SwordSkill sword { get; private set; }
     public BlackholeSkill blackhole { get; private set; }
-
+    public CrystalSkill crystal { get; private set; }
     private void Awake()
     {
         if (instance != null)
@@ -24,5 +24,6 @@ public class SkillManager : MonoBehaviour
         clone = GetComponent<CloneSkill>();
         sword = GetComponent<SwordSkill>();
         blackhole = GetComponent<BlackholeSkill>();
+        crystal = GetComponent<CrystalSkill>();
     }
 }
